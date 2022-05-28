8111e725bb06e90be8357170dc9b1eb0b8545148
pablito <55856800+probablypablito@users.noreply.github.com>
2022-05-28 15:08:22 -0400

    add support for multiple servers

--- main.py
+++ main.py
@@ -25,30 +25,50 @@ def get_holidays():
 
 # Discord Bot
 bot = commands.Bot(command_prefix="!checkiday ")
-channel_id = 0
+channel_list = {}
+
+# Sets value in json to guild id upon the bot joining the guild
+@bot.event
+async def on_guild_join(guild):
+    #loads json file to dictionary
+    with open("db.json", "r") as f:
+        guildInfo = json.load(f)
+
+    guildInfo[guild.id] = guild.text_channels[0] #sets key to guilds id and value to top textchannel
+    
+    #writes dictionary to json file
+    with open("db.json", "w") as f:
+        json.dump(guildInfo, f)
+
 
 # Commands
 
 @bot.command()
 async def channelid(ctx, arg):
-    global channel_id
-    channel_id = int(arg)
-    await ctx.send(f"Set channel to <#{arg}>")
-    print(channel_id)
+    with open("db.json", "r") as f:
+        guildInfo = json.load(f)
+    guildInfo[ctx.message.guild.id] = arg
+    with open("db.json", "w") as f:
+        json.dump(guildInfo, f)
 
 
 
 @tasks.loop(seconds=30)
 async def called_once_a_day():
-    if bot.get_channel(channel_id) == None: return 
-    
-    message_channel = bot.get_channel(channel_id)
-    print(f"Got channel {message_channel}")
+   
     holidays = get_holidays()
     # Send the message
-    try:
-        await message_channel.send(f"__Today's Holidays__\n\n{holidays}")
-    except: print("error!")
+    with open("db.json", "r") as f:
+        guildInfo = json.load(f)
+
+    for i in guildInfo:
+        channel = bot.get_channel(int(guildInfo[i]))
+        try:
+            print("------------")
+            print(f"channel: {channel}")
+            print(f"guildInfo[i]: {guildInfo[i]}")
+            await channel.send(f"__Today's Holidays__\n\n{holidays}")
+        except: print("error!")
 
 
 @called_once_a_day.before_loop
@@ -68,22 +88,3 @@ f.close
 
 
 bot.run(token)
-
-
-
-
-
-
-
-
-
-
-
-
-today = f"{date.month}/{date.day}/{date.year}"
-response = requests.get(f"https://www.checkiday.com/api/3/?d={today}")
-holidays_list = json.loads(response.content)["holidays"]
-
-for i in range(len(holidays_list)):
-    holiday = holidays_list[i]
-    print(holiday["name"])
