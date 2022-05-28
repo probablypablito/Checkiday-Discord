0fdb2f2a6467f9003acb378ac4d6a0bd61ed4127
pablito <55856800+probablypablito@users.noreply.github.com>
2022-05-28 13:11:26 -0400

    Reformat code a little bit

--- main.py
+++ main.py
@@ -6,26 +6,9 @@ import json
 from discord.ext import commands, tasks
 
 
-# Discord Bot
-bot = commands.Bot(command_prefix="!checkiday ")
-
-# Commands
-
-@bot.command()
-async def channelid(ctx, arg):
-    global channel_id
-    channel_id = int(arg)
-    await ctx.send(f"Set channel id to {arg}")
-
-
-
-
-@tasks.loop(seconds=30)
-async def called_once_a_day():
-    message_channel = bot.get_channel(channel_id)
-    print(f"Got channel {message_channel}")
-    
-    # Get the list of holidays
+# Get today's Holidays
+def get_holidays():
+      # Get the list of holidays
     holidays = []
     date = datetime.datetime.now()
     today = f"{date.month}/{date.day}/{date.year}"
@@ -38,9 +21,35 @@ async def called_once_a_day():
         holidays.append(holiday["name"])
 
     holidays = "\n".join(holidays)
-   # print(holidays)
-    if (channel_id):
+    return holidays
+
+# Discord Bot
+bot = commands.Bot(command_prefix="!checkiday ")
+channel_id = 0
+
+# Commands
+
+@bot.command()
+async def channelid(ctx, arg):
+    global channel_id
+    channel_id = int(arg)
+    await ctx.send(f"Set channel to <#{arg}>")
+    print(channel_id)
+
+
+
+@tasks.loop(seconds=30)
+async def called_once_a_day():
+    if bot.get_channel(channel_id) == None: return 
+    
+    message_channel = bot.get_channel(channel_id)
+    print(f"Got channel {message_channel}")
+    holidays = get_holidays()
+    # Send the message
+    try:
         await message_channel.send(f"__Today's Holidays__\n\n{holidays}")
+    except: print("error!")
+
 
 @called_once_a_day.before_loop
 async def before():
